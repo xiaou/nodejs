dc92f92044249f94da7cc9bf3bfa75379fededef
xiaou <xiaou@xiaou-K50IN.(none)>
2012-09-11 01:22:53 +0800

    a

---
+++ httpServer/wsServer.js
@@ -0,0 +1,28 @@
+// wsServer.js
+
+
+var ws = require("websocket-server");
+var server = ws.createServer({debug: true});
+
+
+server.addListener("connection", 
+function(connection)
+{
+	connection.addListener("message",
+	function(message)
+	{
+		server.broadcast(message);
+	}
+	);
+}
+);
+
+server.addListener("close",
+
+
+server.listen(8091);
+
+
+
+
+
